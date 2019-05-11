42873cb85999c3c98c0623634b58352b66f00ea4
GerrySoft <gerard@ricebes.cat>
2019-05-11 20:43:24 +0200

    Capturar datos de una web
    Ejemplo para capturar información de una web, en este caso lo haremos para recopilar información sobre un vídeo en YouTube (título, descripción, likes, canal...)

---
+++ C# - Capturar datos de una web/frmvideo.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace c_sahrp
+{
+    partial class frmvideo
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblikes = new System.Windows.Forms.Label();
+            this.lbdislikes = new System.Windows.Forms.Label();
+            this.lbvisualizaciones = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbcanal = new System.Windows.Forms.Label();
+            this.lbtitulo = new System.Windows.Forms.Label();
+            this.lbcategoria = new System.Windows.Forms.Label();
+            this.lbsuscriptores = new System.Windows.Forms.Label();
+            this.pbcanal = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.rtbdescripcion = new System.Windows.Forms.RichTextBox();
+            this.txtlikes = new System.Windows.Forms.TextBox();
+            this.txtdislikes = new System.Windows.Forms.TextBox();
+            this.txtvisualizaciones = new System.Windows.Forms.TextBox();
+            this.txttitulo = new System.Windows.Forms.TextBox();
+            this.txtcategoria = new System.Windows.Forms.TextBox();
+            this.txtcanal = new System.Windows.Forms.TextBox();
+            this.txtsuscriptores = new System.Windows.Forms.TextBox();
+            this.txtfecha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.btncapturar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcanal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblikes
+            // 
+            this.lblikes.AutoSize = true;
+            this.lblikes.Location = new System.Drawing.Point(12, 83);
+            this.lblikes.Name = "lblikes";
+            this.lblikes.Size = new System.Drawing.Size(82, 29);
+            this.lblikes.TabIndex = 0;
+            this.lblikes.Text = "Likes: ";
+            // 
+            // lbdislikes
+            // 
+            this.lbdislikes.AutoSize = true;
+            this.lbdislikes.Location = new System.Drawing.Point(12, 127);
+            this.lbdislikes.Name = "lbdislikes";
+            this.lbdislikes.Size = new System.Drawing.Size(110, 29);
+            this.lbdislikes.TabIndex = 1;
+            this.lbdislikes.Text = "Dislikes: ";
+            // 
+            // lbvisualizaciones
+            // 
+            this.lbvisualizaciones.AutoSize = true;
+            this.lbvisualizaciones.Location = new System.Drawing.Point(12, 177);
+            this.lbvisualizaciones.Name = "lbvisualizaciones";
+            this.lbvisualizaciones.Size = new System.Drawing.Size(191, 29);
+            this.lbvisualizaciones.TabIndex = 2;
+            this.lbvisualizaciones.Text = "Visualizaciones: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(485, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-----------------------------------------------------------";
+            // 
+            // lbcanal
+            // 
+            this.lbcanal.AutoSize = true;
+            this.lbcanal.Location = new System.Drawing.Point(12, 398);
+            this.lbcanal.Name = "lbcanal";
+            this.lbcanal.Size = new System.Drawing.Size(216, 29);
+            this.lbcanal.TabIndex = 4;
+            this.lbcanal.Text = "Nombre del canal: ";
+            // 
+            // lbtitulo
+            // 
+            this.lbtitulo.AutoSize = true;
+            this.lbtitulo.Location = new System.Drawing.Point(12, 226);
+            this.lbtitulo.Name = "lbtitulo";
+            this.lbtitulo.Size = new System.Drawing.Size(218, 29);
+            this.lbtitulo.TabIndex = 3;
+            this.lbtitulo.Text = "Nombre del vídeo: ";
+            // 
+            // lbcategoria
+            // 
+            this.lbcategoria.AutoSize = true;
+            this.lbcategoria.Location = new System.Drawing.Point(12, 273);
+            this.lbcategoria.Name = "lbcategoria";
+            this.lbcategoria.Size = new System.Drawing.Size(235, 29);
+            this.lbcategoria.TabIndex = 6;
+            this.lbcategoria.Text = "Categoría del vídeo: ";
+            // 
+            // lbsuscriptores
+            // 
+            this.lbsuscriptores.AutoSize = true;
+            this.lbsuscriptores.Location = new System.Drawing.Point(12, 445);
+            this.lbsuscriptores.Name = "lbsuscriptores";
+            this.lbsuscriptores.Size = new System.Drawing.Size(222, 29);
+            this.lbsuscriptores.TabIndex = 7;
+            this.lbsuscriptores.Text = "Nº de suscriptores: ";
+            // 
+            // pbcanal
+            // 
+            this.pbcanal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbcanal.Location = new System.Drawing.Point(147, 487);
+            this.pbcanal.Name = "pbcanal";
+            this.pbcanal.Size = new System.Drawing.Size(100, 100);
+            this.pbcanal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbcanal.TabIndex = 8;
+            this.pbcanal.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 487);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 87);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Imagen\r\ndel\r\ncanal:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(549, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 29);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Descripción:";
+            // 
+            // rtbdescripcion
+            // 
+            this.rtbdescripcion.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rtbdescripcion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbdescripcion.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.rtbdescripcion.Location = new System.Drawing.Point(554, 127);
+            this.rtbdescripcion.Name = "rtbdescripcion";
+            this.rtbdescripcion.ReadOnly = true;
+            this.rtbdescripcion.Size = new System.Drawing.Size(408, 468);
+            this.rtbdescripcion.TabIndex = 11;
+            this.rtbdescripcion.Text = "";
+            // 
+            // txtlikes
+            // 
+            this.txtlikes.Location = new System.Drawing.Point(100, 80);
+            this.txtlikes.Name = "txtlikes";
+            this.txtlikes.Size = new System.Drawing.Size(397, 34);
+            this.txtlikes.TabIndex = 12;
+            // 
+            // txtdislikes
+            // 
+            this.txtdislikes.Location = new System.Drawing.Point(128, 127);
+            this.txtdislikes.Name = "txtdislikes";
+            this.txtdislikes.Size = new System.Drawing.Size(369, 34);
+            this.txtdislikes.TabIndex = 13;
+            // 
+            // txtvisualizaciones
+            // 
+            this.txtvisualizaciones.Location = new System.Drawing.Point(203, 174);
+            this.txtvisualizaciones.Name = "txtvisualizaciones";
+            this.txtvisualizaciones.Size = new System.Drawing.Size(294, 34);
+            this.txtvisualizaciones.TabIndex = 14;
+            // 
+            // txttitulo
+            // 
+            this.txttitulo.Location = new System.Drawing.Point(236, 226);
+            this.txttitulo.Name = "txttitulo";
+            this.txttitulo.Size = new System.Drawing.Size(261, 34);
+            this.txttitulo.TabIndex = 15;
+            // 
+            // txtcategoria
+            // 
+            this.txtcategoria.Location = new System.Drawing.Point(253, 273);
+            this.txtcategoria.Name = "txtcategoria";
+            this.txtcategoria.Size = new System.Drawing.Size(244, 34);
+            this.txtcategoria.TabIndex = 16;
+            // 
+            // txtcanal
+            // 
+            this.txtcanal.Location = new System.Drawing.Point(234, 398);
+            this.txtcanal.Name = "txtcanal";
+            this.txtcanal.Size = new System.Drawing.Size(263, 34);
+            this.txtcanal.TabIndex = 17;
+            // 
+            // txtsuscriptores
+            // 
+            this.txtsuscriptores.Location = new System.Drawing.Point(236, 442);
+            this.txtsuscriptores.Name = "txtsuscriptores";
+            this.txtsuscriptores.Size = new System.Drawing.Size(261, 34);
+            this.txtsuscriptores.TabIndex = 18;
+            // 
+            // txtfecha
+            // 
+            this.txtfecha.Location = new System.Drawing.Point(253, 316);
+            this.txtfecha.Name = "txtfecha";
+            this.txtfecha.Size = new System.Drawing.Size(244, 34);
+            this.txtfecha.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 29);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Fecha de publicaión: ";
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(17, 12);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(742, 34);
+            this.txturl.TabIndex = 22;
+            this.txturl.Text = "https://www.youtube.com/watch?v=";
+            // 
+            // btncapturar
+            // 
+            this.btncapturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncapturar.Location = new System.Drawing.Point(767, 12);
+            this.btncapturar.Name = "btncapturar";
+            this.btncapturar.Size = new System.Drawing.Size(195, 34);
+            this.btncapturar.TabIndex = 23;
+            this.btncapturar.Text = "Captar datos";
+            this.btncapturar.UseVisualStyleBackColor = true;
+            this.btncapturar.Click += new System.EventHandler(this.btncapturar_Click);
+            // 
+            // frmvideo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(993, 616);
+            this.Controls.Add(this.btncapturar);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.txtfecha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsuscriptores);
+            this.Controls.Add(this.txtcanal);
+            this.Controls.Add(this.txtcategoria);
+            this.Controls.Add(this.txttitulo);
+            this.Controls.Add(this.txtvisualizaciones);
+            this.Controls.Add(this.txtdislikes);
+            this.Controls.Add(this.txtlikes);
+            this.Controls.Add(this.rtbdescripcion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pbcanal);
+            this.Controls.Add(this.lbsuscriptores);
+            this.Controls.Add(this.lbcategoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbcanal);
+            this.Controls.Add(this.lbtitulo);
+            this.Controls.Add(this.lbvisualizaciones);
+            this.Controls.Add(this.lbdislikes);
+            this.Controls.Add(this.lblikes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmvideo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Captar datos de YouTube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbcanal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblikes;
+        private System.Windows.Forms.Label lbdislikes;
+        private System.Windows.Forms.Label lbvisualizaciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbcanal;
+        private System.Windows.Forms.Label lbtitulo;
+        private System.Windows.Forms.Label lbcategoria;
+        private System.Windows.Forms.Label lbsuscriptores;
+        private System.Windows.Forms.PictureBox pbcanal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox rtbdescripcion;
+        private System.Windows.Forms.TextBox txtlikes;
+        private System.Windows.Forms.TextBox txtdislikes;
+        private System.Windows.Forms.TextBox txtvisualizaciones;
+        private System.Windows.Forms.TextBox txttitulo;
+        private System.Windows.Forms.TextBox txtcategoria;
+        private System.Windows.Forms.TextBox txtcanal;
+        private System.Windows.Forms.TextBox txtsuscriptores;
+        private System.Windows.Forms.TextBox txtfecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.Button btncapturar;
+    }
+}
---
+++ C# - Capturar datos de una web/frmvideo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_sahrp
+{
+    public partial class frmvideo : Form
+    {
+        public frmvideo()
+        {
+            InitializeComponent();   
+        }
+
+        WebBrowser navegador = new WebBrowser();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            navegador.ScriptErrorsSuppressed = true;
+            navegador.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.datos_cargados);
+        }
+
+        private void datos_cargados(object sender, EventArgs e)
+        {
+            try
+            {
+                txttitulo.Text = navegador.Document.GetElementById("eow-title").InnerText;
+                rtbdescripcion.Text = navegador.Document.GetElementById("eow-description").InnerText;
+
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("g-hovercard yt-uix-sessionlink      spf-link "))
+                    {
+                        txtcategoria.Text = etiqueta.InnerText;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count"))
+                    {
+                        txtsuscriptores.Text = etiqueta.InnerText;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-like-button like-button-renderer-like-button-unclicked yt-uix-clickcard-target   yt-uix-tooltip"))
+                    {
+                        txtlikes.Text = etiqueta.InnerText;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-dislike-button like-button-renderer-dislike-button-unclicked yt-uix-clickcard-target   yt-uix-tooltip"))
+                    {
+                        txtdislikes.Text = etiqueta.InnerText;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("watch-view-count"))
+                    {
+                        txtvisualizaciones.Text = etiqueta.InnerText;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("watch-time-text"))
+                    {
+                        txtfecha.Text = etiqueta.InnerText;
+                    }
+                }
+                int a = 0;
+                foreach (HtmlElement etiqueta in navegador.Document.All)
+                {
+                    if (etiqueta.GetAttribute("classname").Contains("g-hovercard yt-uix-sessionlink      spf-link ") && a == 0)
+                    {
+                        txtcanal.Text = etiqueta.InnerText;
+                        a += 1;
+                    }
+                }
+                foreach (HtmlElement etiqueta in navegador.Document.GetElementsByTagName("img"))
+                {
+                    if (etiqueta.GetAttribute("alt").Contains(txtcanal.Text))
+                    {
+                        pbcanal.ImageLocation = etiqueta.GetAttribute("data-thumb");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private void btncapturar_Click(object sender, EventArgs e)
+        {
+            navegador.Navigate(txturl.Text);
+        }
+    }
+}
+
+
+
